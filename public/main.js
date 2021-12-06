f51cd1681a37208eae987be0797b705c9858c8db
nmssalman <nmssalman@outlook.com>
2021-12-06 13:34:49 -0700

    fixes

--- public/main.js
+++ public/main.js
@@ -241,7 +241,7 @@ $(function() {
   socket.on('login', (data) => {
     connected = true;
     // Display the welcome message
-    const message = 'Welcome to Socket.IO Chat – ';
+    const message = 'Welcome to Enroute Live Tracking – ';
     log(message, {
       prepend: true
     });
